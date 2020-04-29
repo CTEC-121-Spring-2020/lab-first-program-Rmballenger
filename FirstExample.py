82ac3751fa3e6e06f4a218af24c7e1e156953bdf
Rmballenger <R.ballenger@students.clark.edu>
2020-04-29 12:23:29 -0700

    Updated

--- FirstExample.py
+++ FirstExample.py
@@ -11,7 +11,10 @@ Process: description of what function does
 Output: return value and description
 """
 
-def main():
-    print("I'm not super great when it comes to ASCII art. However I have now made the exmple code file. yay!")
 
-main()    
+def main():
+    print("Sam took", 2+2, "apples from the store.")
+    #print("I'm not super great when it comes to ASCII art. However I have now made the exmple code file. yay!")
+
+
+main()
